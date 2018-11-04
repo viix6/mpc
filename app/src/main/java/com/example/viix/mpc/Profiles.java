9e92b81a2d71175a7fd53dc610e90f2ffeea3e1b
Vii <vii@Violets-MacBook-Air.local>
2018-11-05 02:15:44 +0800

    Added retrieved pet data

--- app/src/main/java/com/example/viix/mpc/Profiles.java
+++ app/src/main/java/com/example/viix/mpc/Profiles.java
@@ -6,8 +6,22 @@ import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import android.support.annotation.NonNull;
+import android.widget.LinearLayout;
+
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.OnCompleteListener;
+
 public class Profiles extends AppCompatActivity {
     Button btnAdd;
+    private LinearLayout linearLayout;
+    private FirebaseFirestore db;
+    private String userUid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,8 +29,10 @@ public class Profiles extends AppCompatActivity {
         setContentView(R.layout.activity_profiles);
 
         btnAdd = findViewById(R.id.btnAdd);
-
-
+        linearLayout = findViewById(R.id.profileLayout);
+        this.db = FirebaseFirestore.getInstance();
+        this.userUid = FirebaseAuth.getInstance().getUid();
+        addPetButtons();
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -26,4 +42,37 @@ public class Profiles extends AppCompatActivity {
 
 
     }
+
+
+
+    private void addPetButtons(){
+        db.collection(this.userUid)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                String petName = (String)document.get("name");
+                                String petID = document.getId();
+                                addPetButton(petName,petID);
+                            }
+                        } else {
+                            //fail
+                        }
+                    }
+                });
+    }
+
+    private void addPetButton(String petName, String petID){
+        Button petButton = new Button(this);
+        petButton.setText(petName);
+        //TODO: add a listener to the button to start the activity where the pet will be display
+        //TODO : pass the petID as a parameter to our activity so we can retrieve the data from the firestore
+        this.linearLayout.addView(petButton);
+
+
+    }
+
+
 }
